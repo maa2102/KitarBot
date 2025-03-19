81540d06565c88484be34d08053cd37e0d08c502
maa2102 <muhd.afiq.asyraf.maa@gmail.com>
2025-03-19 12:47:14 +0800

    change base api

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ from typing import Optional
 from PIL import Image
 import tensorflow as tf
 
-
 # Constant
-BASE_API_URL = "https://f5fe-175-139-159-165.ngrok-free.app"
+BASE_API_URL = "http://127.0.0.1:7860"
 FLOW_ID = "3e733bf0-7649-4b45-af59-989397f3e2e6"
 ENDPOINT = "" # You can set a specific endpoint name in the flow settings
 TWEAKS = {
